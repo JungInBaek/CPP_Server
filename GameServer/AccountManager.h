98afbeeb34cd461ea1dd639e4d4f02d9b5931e0b
JungInBaek <bji1527@naver.com>
2024-04-16 18:41:23 +0900

    feat: Thread Manager 추가

---
+++ GameServer/AccountManager.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <mutex>
+
+
+class Account
+{
+	// TODO
+};
+
+class AccountManager
+{
+public:
+	static AccountManager* Instance()
+	{
+		static AccountManager instance;
+		return &instance;
+	}
+
+	Account* GetAccount(int32 id)
+	{
+		lock_guard<mutex> guard(_mutex);
+		
+		// Account °Ë»ö
+
+		return nullptr;
+	}
+
+	void ProcessLogin();
+
+private:
+	mutex _mutex;
+	 //map<int32, Account*> _accounts;
+};
+
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,7 +1,34 @@
 ﻿#include "pch.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <Windows.h>
+#include "AccountManager.h"
+#include "UserManager.h"
+#include <future>
+#include "ConcurrentStack.h"
+#include "ConcurrentQueue.h"
+#include "ThreadManager.h"
 
+using namespace std;
+
+
+CoreGlobal Core;
+
+void ThreadMain()
+{
+	while (true)
+	{
+		cout << "Hello! I am thread... " << LThreadId << endl;
+		this_thread::sleep_for(1s);
+	}
+}
 
 int main()
 {
-    HelloWorld();
+	for (int i = 0; i < 5; i++)
+	{
+		GThreadManager->Launch(ThreadMain);
+	}
+	GThreadManager->Join();
 }
