844f072bc6bcbf9d09b08fd7188fb6d6d7c50194
unknown <bji0519@gmail.com>
2024-07-26 23:46:05 +0900

    feat: ORM 구현

--- ServerCore/CoreGlobal.cpp
+++ ServerCore/CoreGlobal.cpp
@@ -10,6 +10,7 @@
 #include "GlobalQueue.h"
 #include "JobTimer.h"
 #include "DBConnectionPool.h"
+#include "ConsoleLog.h"
 
 
 ThreadManager* GThreadManager = nullptr;
@@ -20,6 +21,7 @@ JobTimer* GJobTimer = nullptr;
 
 DeadLockProfiler* GDeadLockProfiler = nullptr;
 DBConnectionPool* GDBConnectionPool = nullptr;
+ConsoleLog* GConsoleLogger = nullptr;
 
 
 class CoreGlobal
@@ -34,6 +36,7 @@ public:
 		GJobTimer = new JobTimer();
 		GDeadLockProfiler = new DeadLockProfiler();
 		GDBConnectionPool = new DBConnectionPool();
+		GConsoleLogger = new ConsoleLog();
 		SocketUtils::Init();
 	}
 
@@ -46,6 +49,7 @@ public:
 		delete GJobTimer;
 		delete GDeadLockProfiler;
 		delete GDBConnectionPool;
+		delete GConsoleLogger;
 		SocketUtils::Clear();
 	}
 } GCoreGlobal;
