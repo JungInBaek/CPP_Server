2fd39795a784e95c79884d4e6d971420909f31a6
unknown <bji0519@gmail.com>
2024-07-27 22:39:39 +0900

    feat: DB 프로시저 함수 코드 생성 자동화

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -12,6 +12,7 @@
 #include "DBBind.h"
 #include "XmlParser.h"
 #include "DBSynchronizer.h"
+#include "GenProcedures.h"
 
 
 enum
@@ -39,10 +40,44 @@ int main()
 	ASSERT_CRASH(GDBConnectionPool->Connect(1, L"Driver={ODBC Driver 17 for SQL Server};Server=(localdb)\\ProjectModels;Database=serverDB;Trusted_Connection=Yes;"));
 
 	DBConnection* dbConn = GDBConnectionPool->Pop();
-
 	DBSynchronizer dbSync(*dbConn);
-
 	dbSync.Synchronize(L"GameDB.xml");
+
+	{
+		WCHAR name[] = L"테스트";
+		SP::InsertGold insertGold(*dbConn);
+
+		insertGold.In_Gold(100);
+		insertGold.In_Name(name);
+		insertGold.In_CreateDate(TIMESTAMP_STRUCT{ 2024, 7, 27 });
+
+		insertGold.Execute();
+	}
+
+	{
+		SP::GetGold getGold(*dbConn);
+
+		getGold.In_Gold(100);
+
+		int32 id = 0;
+		int32 gold = 0;
+		WCHAR name[100];
+		TIMESTAMP_STRUCT createDate;
+
+		getGold.Out_Id(OUT id);
+		getGold.Out_Gold(OUT gold);
+		getGold.Out_Name(OUT name);
+		getGold.Out_CreateDate(OUT createDate);
+
+		getGold.Execute();
+
+		setlocale(LC_ALL, "");
+		while (getGold.Fetch())
+		{
+			GConsoleLogger->WriteStdOut(Color::BLUE,
+				L"ID[%d] GOLD[%d] NAME[%s]\n", id, gold, name);
+		}
+	}
 
 	ClientPacketHandler::Init();
 
